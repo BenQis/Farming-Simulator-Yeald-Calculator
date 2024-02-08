230c1d083f0dff845f76cfbfc10609f3995b9df3
Jakub Konop <kbkonop@gmail.com>
2024-02-08 21:58:20 +0100

    Basic program working version 0.0.1

---
+++ Database_dictionary.py
@@ -0,0 +1,114 @@
+Easy_prices = {'Pszenica': [1011, 1314, 1415, 'Styczeń, Grudzień'],                               # Pszenica
+               'Jęczmień': [939, 1221, 1315,  'Styczeń Grudzień'],                                # Jęczmień
+               'Owies': [1596, 2075, 2234,  'Styczeń Grudzień'],                                  # Owies
+               'Rzepak': [1809, 2352, 2533, 'Grudzień, Listopad'],                                # Rzepak
+               'Sorgo': [1290, 1677, 1806, 'Styczeń, Luty'],                                      # Sorgo
+               'Winogrono': [1809, 2352, 2533, 'Maj, Czerwiec'],                                  # Winogrona
+               'Oliwki': [1809, 2352, 2533, 'Czerwiec, Lipiec'],                                  # Oliwki
+               'Słonecznik': [2019, 2625, 2827, 'Marzec, Kwiecień'],                              # Słonecznik
+               'Soja': [2333, 3033, 3266, 'Lipiec, Czerwiec'],                                    # Soja
+               'Kukurydza': [1139, 1481, 1595, 'Styczeń, Lipiec'],                                # Kukurydza
+               'Ziemniaki': [516, 671, 722, 'Styczeń, Luty'],                                     # Ziemniaki
+               'Buraki cukrowe': [366, 476, 512, 'Styczeń, Luty'],                                # Buraki cukrowe
+               'Bawełna': [3756, 4883, 5258, 'Marzec, Luty'],                                     # Bawełna
+               'Trzcina cukrowa': [357, 464, 500, 'Lipiec, Wrzesień, Sierpień, Październik'],     # Trzcina cukrowa
+               'Topola': [126, 164, 176, 'Luty, Marzec'],                                         # Topola
+               'Trawa': [135, 176, 189, 'Styczeń, Luty, Grudzień'],                               # Trawa
+               'Słoma': [123, 160, 172, 'Styczeń, Luty, Grudzień']}                               # Słoma
+
+Normal_prices = {'Pszenica': [607, 789, 850, 'Styczeń, Grudzień'],                                # Pszenica
+                 'Jęczmień': [563, 732, 788,  'Styczeń Grudzień'],                                # Jęczmień
+                 'Owies': [958, 1245, 1341,  'Styczeń Grudzień'],                                 # Owies
+                 'Rzepak': [1085, 1411, 1519, 'Grudzień, Listopad'],                              # Rzepak
+                 'Sorgo': [773, 1006, 1084, 'Styczeń, Luty'],                                     # Sorgo
+                 'Winogrono': [1085, 1411, 1519, 'Maj, Czerwiec'],                                # Winogrona
+                 'Oliwki': [1085, 1411, 1519, 'Czerwiec, Lipiec'],                                # Oliwki
+                 'Słonecznik': [1211, 1574, 1695, 'Marzec, Kwiecień'],                            # Słonecznik
+                 'Soja': [1400, 1820, 1960, 'Lipiec, Czerwiec'],                                  # Soja
+                 'Kukurydza': [683, 888, 956, 'Styczeń, Lipiec'],                                 # Kukurydza
+                 'Ziemniaki': [310, 403, 434, 'Styczeń, Luty'],                                   # Ziemniaki
+                 'Buraki cukrowe': [220, 286, 308, 'Styczeń, Luty'],                              # Buraki cukrowe
+                 'Bawełna': [2254, 2930, 3156, 'Marzec, Luty'],                                   # Bawełna
+                 'Trzcina cukrowa': [214, 278, 300, 'Lipiec, Wrzesień, Sierpień, Październik'],   # Trzcina cukrowa
+                 'Topola': [76, 99, 106, 'Luty, Marzec'],                                         # Topola
+                 'Trawa': [81, 105, 113, 'Styczeń, Luty, Grudzień'],                              # Trawa
+                 'Słoma': [74, 96, 104, 'Styczeń, Luty, Grudzień']}                               # Słoma
+
+Hard_prices = {'Pszenica': [337, 438, 472, 'Styczeń, Grudzień'],                                  # Pszenica
+               'Jęczmień': [313, 407, 438,  'Styczeń Grudzień'],                                  # Jęczmień
+               'Owies': [532, 692, 745,  'Styczeń Grudzień'],                                     # Owies
+               'Rzepak': [603, 784, 844, 'Grudzień, Listopad'],                                   # Rzepak
+               'Sorgo': [430, 559, 602, 'Styczeń, Luty'],                                         # Sorgo
+               'Winogrono': [603, 784, 844, 'Maj, Czerwiec'],                                     # Winogrona
+               'Oliwki': [603, 784, 844, 'Czerwiec, Lipiec'],                                     # Oliwki
+               'Słonecznik': [673, 875, 942, 'Marzec, Kwiecień'],                                 # Słonecznik
+               'Soja': [778, 1011, 1089, 'Lipiec, Czerwiec'],                                     # Soja
+               'Kukurydza': [380, 494, 532, 'Styczeń, Lipiec'],                                   # Kukurydza
+               'Ziemniaki': [172, 224, 241, 'Styczeń, Luty'],                                     # Ziemniaki
+               'Buraki cukrowe': [122, 159, 171, 'Styczeń, Luty'],                                # Buraki cukrowe
+               'Bawełna': [1252, 1628, 1753, 'Marzec, Luty'],                                     # Bawełna
+               'Trzcina cukrowa': [119, 155, 167, 'Lipiec, Wrzesień, Sierpień, Październik'],     # Trzcina cukrowa
+               'Topola': [42, 55, 59, 'Luty, Marzec'],                                            # Topola
+               'Trawa': [45, 59, 63, 'Styczeń, Luty, Grudzień'],                                  # Trawa
+               'Słoma': [41, 53, 57, 'Styczeń, Luty, Grudzień']}                                  # Słoma
+
+crop_harvest = {'Pszenica': [8900, 73447],       # Pszenica
+                'Jęczmień': [9600, 73528],       # Jęczmień
+                'Owies': [5700, 73594],          # Owies
+                'Rzepak': [5800, 0],             # Rzepak
+                'Sorgo': [8200, 0],              # Sorgo
+                'Winogrono': [9200, 0],          # Winogrono
+                'Oliwki': [9200, 0],             # Oliwki
+                'Słonecznik': [5200, 0],         # Słonecznik
+                'Soja': [4500, 0],               # Soja
+                'Kukurydza': [9200, 0],          # Kukurydza
+                'Ziemniaki': [41300, 0],         # Ziemniaki
+                'Buraki cukrowe': [57800, 0],    # Buraki Cukrowe
+                'Bawełna': [4970, 0],            # Bawełna
+                'Trzcina cukrowa': [113400, 0],  # Trzcina Cukrowa
+                'Topola': [71760, 0],            # Topola
+                'Trawa': [32800, 0]}             # Trawa
+
+seed_usage = {'Pszenica': 500,           # Pszenica
+              'Jęczmień': 500,           # Jęczmień
+              'Owies': 500,              # Owies
+              'Rzepak': 200,             # Rzepak
+              'Sorgo': 200,              # Sorgo
+              'Słonecznik': 300,         # Słonecznik
+              'Soja': 300,               # Soja
+              'Kukurydza': 400,          # Kukurydza
+              'Ziemniaki': 3800,         # Ziemniaki
+              'Buraki cukrowe': 400,     # Buraki Cukrowe
+              'Bawełna': 500,            # Bawełna
+              'Trzcina cukrowa': 12000,  # Trzcina Cukrowa
+              'Topola': 1500,            # Topola
+              'Trawa': 300,              # Trawa
+              'Rzodkiew oleista': 100}   # Rzodkiew oleista
+# radio_var1
+manure = {'Gnojówka': [14390, 0],        # Gnojówka {}, koszt za litr
+          # 'Obornik': [14390, 0],       # Obornik {}, koszt za litr
+          # 'Kompost': [14390, 0],       # Kompost {}, koszt za litr
+          'Wapno': [3237, 0.175],        # Wapno {}, koszt za litr
+          'Nawóz stały': [215, 1.82],    # Nawóz stały {}, koszt za litr
+          'Nawóz płynny': [300, 1.6],    # Nawóz płynny {}, koszt za litr
+          1: [300, 1.2]}                 # Herbicyd {}, koszt za litr
+
+crop_data = ['Pszenica',
+             'Jęczmień',
+             'Owies',
+             'Rzepak',
+             'Sorgo',
+             'Winogrono',
+             'Oliwki',
+             'Słonecznik',
+             'Soja',
+             'Kukurydza',
+             'Ziemniaki',
+             'Buraki Cukrowe',
+             'Bawełna',
+             'Trzcina Cukrowa',
+             'Topola',
+             'Trawa',
+             'Słoma']
+
+seed_price = 0.9  # cena za 1 l nasion dla worka BIG BAG
